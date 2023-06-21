6b4282c78c7a78d5bc8ff2b9db013f27a1e3ab66
sandkalam <sandykalamuddin07@gmail.com>
2023-06-21 20:55:40 +0700

    🎉 init:

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+// app
+
+export default function App() {
+  return (
+    <>
+      <div className="container mx-auto">
+        <h1>Vite + React</h1>
+        <p>
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quisquam,
+          aliquam laborum fugit architecto tempora magni, error odit recusandae
+          numquam illum ducimus eaque dolorum repudiandae iste earum? A saepe
+          perferendis aliquam!
+        </p>
+      </div>
+    </>
+  );
+}
